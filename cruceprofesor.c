51eb5a87d50e36478a4dff45376ee169f12bc160
Alejandro Garau Madrigal <alec.augal@gmail.com>
2020-02-08 16:55:42 +0100

    Finished with the specs the teacher specified

--- cruceprofesor.c
+++ cruceprofesor.c
@@ -2,31 +2,44 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdbool.h>
 
 pthread_mutex_t mutex;
-pthread_cond_t pueden_pasar_horizontal, pueden_pasar_vertical;
-int semaforo_vertical_verde, semaforo_horizontal_verde;
+pthread_cond_t pueden_pasar_horizontal, pueden_pasar_vertical, horizontal_ha_pasado, vertical_ha_pasado;
+int semaforo_vertical_verde, semaforo_horizontal_verde, coches_esperando_vertical, coches_esperando_horizontal;
 
 
 void* controlador() {
     semaforo_vertical_verde = 0;
     semaforo_horizontal_verde = 1;
     while(1) {
-        semaforo_horizontal_verde = !semaforo_horizontal_verde;
-        if(semaforo_horizontal_verde) {
-            printf("Semaforo horizontal en verde\n");
-            pthread_cond_broadcast(&pueden_pasar_horizontal);
-        } else {
-            printf("Semaforo horizontal en rojo\n");
+        sleep(rand() % 9 + 1);
+        pthread_mutex_lock(&mutex);
+        while(coches_esperando_horizontal > 0) {
+            pthread_cond_wait(&horizontal_ha_pasado, &mutex);
         }
-        semaforo_vertical_verde = !semaforo_vertical_verde;
-        if(semaforo_vertical_verde) {
-            printf("Semaforo vertical en verde\n");
-            pthread_cond_broadcast(&pueden_pasar_vertical);
-        } else {
-            printf("Semaforo vertical en rojo\n");
+
+        printf("Semaforo Vertical en verde (Vertical %d) (Horizontal %d)\n", coches_esperando_vertical, coches_esperando_horizontal);
+        pthread_cond_broadcast(&pueden_pasar_vertical);
+        semaforo_vertical_verde = true;
+        semaforo_horizontal_verde = false;
+        
+        pthread_mutex_unlock(&mutex);
+        sleep(rand() % 9 + 1);
+        pthread_mutex_lock(&mutex);
+
+        while(coches_esperando_vertical > 0) {
+            pthread_cond_wait(&vertical_ha_pasado, &mutex);
+            
         }
-        sleep(10);   
+        
+        printf("Semaforo horizontal en verde. (Vertical %d) (Horizontal %d)\n", coches_esperando_vertical, coches_esperando_horizontal);
+        pthread_cond_broadcast(&pueden_pasar_horizontal);
+        
+        semaforo_vertical_verde = false;
+        semaforo_horizontal_verde = true;
+
+        pthread_mutex_unlock(&mutex);
     }
 }
 
@@ -34,12 +47,18 @@ void* coches_vertical(void* argg) {
     int id = *(int*) argg;
     while(1) {
         pthread_mutex_lock(&mutex);
+        coches_esperando_vertical++;
         while(!semaforo_vertical_verde) {
             printf("Coche %d esperando en el semaforo vertical\n", id);
             pthread_cond_wait(&pueden_pasar_vertical, &mutex);
         }
         pthread_mutex_unlock(&mutex);
+        sleep(rand() % 1 + 1);
+        pthread_mutex_lock(&mutex);
+        coches_esperando_vertical--;
+        pthread_mutex_unlock(&mutex);
         printf("Coche %d pasando por el semaforo vertical\n", id);
+        pthread_cond_signal(&vertical_ha_pasado);
         sleep(rand() % 4 + 1);
     }
     return 0;
@@ -49,12 +68,18 @@ void* coches_horizontal(void* argg) {
     int id = *(int*) argg;
     while(1) {
         pthread_mutex_lock(&mutex);
+        coches_esperando_horizontal++;
         while(!semaforo_horizontal_verde) {
             printf("Coche %d esperando en el semaforo horizontal\n", id);
             pthread_cond_wait(&pueden_pasar_horizontal, &mutex);
         }
         pthread_mutex_unlock(&mutex);
+        sleep(rand() % 1 + 1);
+        pthread_mutex_lock(&mutex);
+        coches_esperando_horizontal--;
+        pthread_mutex_unlock(&mutex);
         printf("Coche %d pasando por el semaforo horizontal\n", id);
+        pthread_cond_signal(&horizontal_ha_pasado);
         sleep(rand() % 4 + 1);
     }
     return 0;
@@ -69,6 +94,8 @@ int main() {
     pthread_mutex_init(&mutex, NULL);
     pthread_cond_init(&pueden_pasar_horizontal, NULL);
     pthread_cond_init(&pueden_pasar_vertical, NULL);
+    pthread_cond_init(&vertical_ha_pasado, NULL);
+    pthread_cond_init(&horizontal_ha_pasado, NULL);
 
     pthread_create(&controller, NULL, controlador, NULL);
 
